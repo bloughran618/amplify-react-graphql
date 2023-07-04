d761a4321023a10630a8f56ea4ce9691aab1d709
Brian Loughran <bloughran618@gmail.com>
2023-07-04 13:10:06 -0400

    add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "amplifyImageStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
